f6367e38519d8ccb19f8a225a07aff119f48e706
Tom <tom@hardindd.com>
2016-06-13 19:05:19 -0500

    summer league

--- data/seasons.js
+++ data/seasons.js
@@ -1,4 +1,5 @@
 module.exports = [
 	{id:1, name: 'Fall 2015', leagueid: 562},
-	{id:2, name: 'Spring 2016', leagueid: 572}
+	{id:2, name: 'Spring 2016', leagueid: 572},
+	{id:3, name: 'Summer 2016', leagueid: 599}
 ]
