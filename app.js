e1f5618bfcb30bd8d087c3c49ad72289d4ca663d
Tom <tom@hardindd.com>
2016-07-12 17:38:29 -0500

    v2 announcements

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 
 var apiRoutes = require('./routes/api');
+var apiRoutesv2 = require('./routes/v2api');
 
 var app = express();
 app.use(cors());
@@ -23,6 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/v1', apiRoutes);
+app.use('/api/v2', apiRoutesv2);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
