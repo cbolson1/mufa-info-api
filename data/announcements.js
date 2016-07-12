b95c7cdbfe256bbb692c6169eabf70e7a3804d1a
Tom <tom@hardindd.com>
2016-07-12 17:29:59 -0500

    announce

--- data/announcements.js
+++ data/announcements.js
@@ -1,4 +1,4 @@
 module.exports = [
 	{content: 'Report bugs to tom@tomj.ninja'},
-	{content: 'ANDROID USERS: This will be moving to a different location in the app store, please download the new one <a href="https://play.google.com/store/apps/details?id=com.tommyj.mufainfo2">HERE</a> as this app WILL STOP WORKING WITHIN 7 DAYS.'}
+	{content: 'ANDROID USERS: This app will be changing locations in the app store, please download the new one <a href="https://play.google.com/store/apps/details?id=com.tommyj.mufainfo2">HERE</a> as this app WILL STOP WORKING WITHIN 7 DAYS.'}
 ]
