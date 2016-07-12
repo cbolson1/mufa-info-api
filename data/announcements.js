f41a7180d8ab62c5600882158cc43b68a8918ce1
Tom <tom@hardindd.com>
2016-07-12 15:32:15 -0500

    announcments

--- data/announcements.js
+++ data/announcements.js
@@ -1,5 +1,3 @@
 module.exports = [
-	{content: 'Report bugs to tom@tomj.ninja'},
-	{content: 'Open source contributions are welcome! http://github.com/tommybananas/mufa-info-ionic'},
-	{content: 'Direction buttons should work again!'}
+	{content: 'Report bugs to tom@tomj.ninja'}
 ]
