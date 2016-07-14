0715b9cbed9a054ff47f1083e3c5bd5cec39ae53
Tom <tom@hardindd.com>
2016-07-13 19:18:13 -0500

    announce

--- data/announcements.js
+++ data/announcements.js
@@ -1,7 +1,7 @@
 module.exports = [
 	{content: 'Report bugs to tom@tomj.ninja'},
-	{version: 1, content: 'URGENT: This version of the app is becoming unsupported soon and will working within 7 days, please see the notes below for your device.'},
+	{version: 1, content: 'URGENT: This version of the app is becoming unsupported soon and will stop working within 7 days, please see the notes below for your device.'},
 	{version: 1, content: 'ANDROID: New play store location - please download the new one in the store:    http://bit.ly/mufainfo or search \'mufa info\''},
-	{version: 1, content: 'IPHONE: If you see this please update your app. This version will be unsupported very soon.'}
+	{version: 1, content: 'IPHONE: If you see this please just update your app. This version will be unsupported very soon.'}
 ]
 ]
