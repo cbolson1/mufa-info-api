6975b7271dfa007bf5b3688dde9cfbe46f9e1afd
Tom <tom@hardindd.com>
2016-07-14 11:14:21 -0500

    announce bug

--- data/announcements.js
+++ data/announcements.js
@@ -4,4 +4,3 @@ module.exports = [
 	{version: 1, content: 'ANDROID: New play store location - please download the new one in the store:    http://bit.ly/mufainfo or search \'mufa info\''},
 	{version: 1, content: 'IPHONE: If you see this please just update your app. This version will be unsupported very soon.'}
 ]
-]
