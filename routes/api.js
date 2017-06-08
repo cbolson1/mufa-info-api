37262088bbbed7aa53cf4861735084e586fed7e8
Tom Juszczyk <tomkj4@gmail.com>
2017-06-08 11:57:36 -0500

    summer fixed

--- routes/api.js
+++ routes/api.js
@@ -80,9 +80,9 @@ router.get('/team/league/:league/team/:team', function(req, res, next) {
 	    	var team_url = cell.eq(4).find('a').attr('href');
 	    	var team_id = querystring.parse(team_url)['/teams/?teamId'];
 
-	    	var field = cell.eq(isSwiss ? 7 : 5).text().split('(')[0].trim();
-	    	var game_time = cell.eq(isSwiss ? 8 : 6).text().trim();
-	    	var color = cell.eq(isSwiss ? 9 : 7).text().trim();
+	    	var field = cell.eq(5).text().split('(')[0].trim();
+	    	var game_time = cell.eq(6).text().trim();
+	    	var color = cell.eq(7).text().trim();
 	    	var location = _.find(_locations, function(l) {
 	    		return field.toUpperCase().indexOf(l.regex.toUpperCase()) >= 0;
 	    	});
