ff296b8aadefc9c304f8a8d07d2596f87ece28be
Tom Juszczyk <tomkj4@gmail.com>
2017-06-08 11:44:11 -0500

    summer 2017

--- data/seasons.js
+++ data/seasons.js
@@ -1,4 +1,5 @@
 module.exports = [
 	{id:3, name: 'Summer 2016', leagueid: 599},
-	{id:4, name: 'Fall 2016', leagueid: 604}
+	{id:4, name: 'Fall 2016', leagueid: 604},
+	{id:5, name: 'Summer 2017', leagueid: 630}
 ]
